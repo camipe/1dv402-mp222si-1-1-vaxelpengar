c46456b043c1c35f3e3dfe1a1419a2b1bf43f63e
Micael Persson <micael.c.persson@gmail.com>
2014-09-10 11:55:37 +0200

    Gjort klart koden för att ta in data och behandla den

--- 1-1-vaxelpengar/Program.cs
+++ 1-1-vaxelpengar/Program.cs
@@ -10,11 +10,15 @@ namespace _1_1_vaxelpengar
     {
         static void Main(string[] args)
         {
-            double subTotal = 0;
-            uint total = 0;
-            double recievedAmount;
-            double roundingOffAmount;
             
+            // Declaring variables
+            double subTotal = 0d;
+            uint recievedAmount;
+            uint total = 0;            
+            double roundingOffAmount;
+            double changeBack;
+            
+            // Request subtotal and recieved amount from user and load them in the variables subTotal and recievedAmount
             Console.Write("Ange totalsumma: ");
             subTotal = double.Parse(Console.ReadLine());
 
@@ -22,14 +26,20 @@ namespace _1_1_vaxelpengar
             recievedAmount = uint.Parse(Console.ReadLine());
 
 
+            // Calculating rounded total, how much is is rounded off and the change
             total = (uint)Math.Round(subTotal);
             roundingOffAmount = total - subTotal;
+            changeBack = recievedAmount - total;
 
-
-            Console.WriteLine("Totalt: {0}", subTotal);
-            Console.WriteLine("Erhållet belopp: {0}", recievedAmount);
-            Console.WriteLine("Att betala: {0}", total);
-            Console.WriteLine("Öresavrunding: {0}", roundingOffAmount);
+            // Print data in console
+            Console.WriteLine("\nKVITTO");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Totalt: {0} kr", subTotal);
+            Console.WriteLine("Öresavrunding: {0:c} kr", roundingOffAmount);
+            Console.WriteLine("Att betala: {0} kr", total);
+            Console.WriteLine("Erhållet belopp(Kontant): {0} kr", recievedAmount);
+            Console.WriteLine("Växel tillbaka: {0} kr", changeBack);
+            Console.WriteLine("-------------------------------------------");
 
 
 
