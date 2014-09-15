d0bc9aa4f1b3e298e9868ad4ac7b24233b75cb50
Micael Persson <micael.c.persson@gmail.com>
2014-09-15 18:49:55 +0200

    Fixed output formatting and error handling.

--- 1-1-vaxelpengar/Program.cs
+++ 1-1-vaxelpengar/Program.cs
@@ -13,21 +13,81 @@ namespace _1_1_vaxelpengar
             
             // Declaring variables
             double subTotal = 0d;
-            uint recievedAmount;
+            uint recievedAmount = 0;
             uint total = 0;            
             double roundingOffAmount;
             uint changeBack;
             
             // Request subtotal and recieved amount from user and load them in the variables subTotal and recievedAmount
-            Console.Write("Ange totalsumma: ");
-            subTotal = double.Parse(Console.ReadLine());
+            while (true)
+            {    
+                try
+                {
+                    Console.Write("Ange totalsumma: ");
+                    subTotal = double.Parse(Console.ReadLine());
 
-            Console.Write("Ange erhållet belopp: ");
-            recievedAmount = uint.Parse(Console.ReadLine());
+                    total = (uint)Math.Round(subTotal);// Calculating rounded total                    
+                    if (total < 1)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Totalsumman är för liten. Köpet kunde inte genomföras.");
+                        Console.ResetColor();
+                        return;
+                    }
+                    else { break; }
+                }
+                catch (System.FormatException)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("FEL! Felaktigt belopp.");
+                    Console.ResetColor();
+                }
+
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Okänt fel. Programmet avslutas.");
+                    Console.ResetColor();
+                    return;
+
+                }
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Ange erhållet belopp: ");
+                    recievedAmount = uint.Parse(Console.ReadLine());
 
 
-            // Calculating rounded total, how much is is rounded off and the change
-            total = (uint)Math.Round(subTotal);
+                    if (recievedAmount < total)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Erhållet belopp är för litet. Köp kunde inte genonföras");
+                        Console.ResetColor();
+                        return;
+                    }
+                    else { break; }
+                }
+
+                catch (System.FormatException)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("FEL! Felaktigt belopp.");
+                    Console.ResetColor();
+                }
+
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Okänt fel. Programmet avslutas.");
+                    Console.ResetColor();
+                    return;
+                }
+            }   
+
+            // Calculating how much is rounded off and the change
+            
             roundingOffAmount = total - subTotal;
             changeBack = recievedAmount - total;
 
@@ -37,15 +97,15 @@ namespace _1_1_vaxelpengar
             // Print data in console
             Console.WriteLine("\nKVITTO");
             Console.WriteLine("----------------------------------");
-            Console.WriteLine("Totalt          :{0, 12} kr", subTotal);
-            Console.WriteLine("Öresavrunding   : {0, 12:c}", roundingOffAmount);
-            Console.WriteLine("Att betala      : {0} kr", total);
-            Console.WriteLine("Erhållet belopp : {0} kr", recievedAmount);
-            Console.WriteLine("Växel tillbaka  : {0} kr", changeBack);
+            Console.WriteLine("Totalt          : {0, 15:c}", subTotal);
+            Console.WriteLine("Öresavrunding   : {0, 15:c}", roundingOffAmount);
+            Console.WriteLine("Att betala      : {0, 15:c}", total);
+            Console.WriteLine("Erhållet belopp : {0, 15:c}", recievedAmount);
+            Console.WriteLine("Växel tillbaka  : {0, 15:c}", changeBack);
             Console.WriteLine("----------------------------------");
 
 
-
+            // Calculating the change in respective denomination
             if ((changeBack / 500) > 0)
             {
                 Console.WriteLine("500-lappar: {0}", (changeBack / 500));
